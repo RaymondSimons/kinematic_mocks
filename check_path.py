8b244af896b84ef6bd68938b503d4f2a67d21a55
RaymondSimons <rsimons@pha.jhu.edu>
2016-08-21 20:38:40 -0400

    testing path

--- check_path.py
+++ check_path.py
@@ -6,7 +6,7 @@ mcrx = glob.glob("mcrx.fits")
 
 
 
-kmap_dir = '/nobackupp2/rcsimons/sunrise_testing/kmaps'
+kmap_dir = '/nobackupp2/rcsimons/sunrise_testing/kmaps/VELA_v2'
 
 
 basepath = os.path.dirname(os.getcwd().replace('/ifu','').replace('_sunrise',''))
@@ -17,6 +17,10 @@ simname = basepath[len(basepath)-6::]
 snapname = os.path.basename(os.getcwd().replace('/ifu','').replace('_sunrise',''))
 
 print simname, snapname
+
+kmap_name = '/nobackupp2/rcsimons/sunrise_testing/kmaps/VELA_v2/'+simname+'/'+snapname
+print kmap_name
+print os.path.lexists('/nobackupp2/rcsimons/sunrise_testing/kmaps/VELA_v2/'+simname)
 
 
 
