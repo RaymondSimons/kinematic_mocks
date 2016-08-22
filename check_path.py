d900835908707bc33dd996ed0a513484046988dd
RaymondSimons <rsimons@pha.jhu.edu>
2016-08-21 20:33:29 -0400

    testing path

--- check_path.py
+++ check_path.py
@@ -7,7 +7,10 @@ mcrx = glob.glob("mcrx.fits")
 
 
 print os.path.basename(os.getcwd().replace('/ifu','').replace('_sunrise',''))
-print os.path.dirname(os.getcwd().replace('/ifu','').replace('_sunrise',''))
+
+basepath = os.path.dirname(os.getcwd().replace('/ifu','').replace('_sunrise',''))
+print basepath[len(basepath)-5::]
+
 
 
 
