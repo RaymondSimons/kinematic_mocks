7a67cb6bd2ec91c07eb1f10e89a8fb5d82a8c5de
RaymondSimons <rsimons@pha.jhu.edu>
2016-08-21 20:29:26 -0400

    testing path

--- check_path.py
+++ check_path.py
@@ -4,5 +4,7 @@ import glob
 
 mcrx = glob.glob("mcrx.fits")
 
-print os.getcwd().replace('/ifu','').replace('_sunrise','')
+print os.path.basename(os.getcwd().replace('/ifu','').replace('_sunrise',''))
+print os.path.dirname(os.getcwd().replace('/ifu','').replace('_sunrise',''))
+
 #print os.basename(os.getcwd().replace('/ifu','').replace('_sunrise',''))
