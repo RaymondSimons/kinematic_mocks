4a96f1619bd897ea72d4eb47be387686b764fbd3
RaymondSimons <rsimons@pha.jhu.edu>
2016-08-25 12:23:38 -0400

    fix

--- save_cubes.py
+++ save_cubes.py
@@ -1,6 +1,6 @@
 import pyfits
 import glob
-
+from numpy import *
 
 files = ['VELA28_a0.%3i_sunrise'%(num) for num in arange(300,380,10)]
 simdir = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA28'
