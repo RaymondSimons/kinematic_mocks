84be06bac811de2e0e1dae456d78bc99a6ae80af
RaymondSimons <rsimons@pha.jhu.edu>
2016-08-25 14:44:29 -0400

    fix

--- save_cubes.py
+++ save_cubes.py
@@ -1,6 +1,8 @@
 import pyfits
 import glob
 from numpy import *
+import os
+
 
 files = ['VELA28_a0.%3i_sunrise'%(num) for num in arange(300,380,10)]
 simdir = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA28'
@@ -8,11 +10,11 @@ for fname in files:
 	fname_fits = fname+'_cam1.fits'
 	print simdir+'/'+fname_fits+'/ifu/mcrx.fits'
 	fle = glob.glob(simdir+'/'+fname+'/ifu/mcrx.fits')[0]
-	mcrx = pyfits.open(fle)
-	data = mcrx['CAMERA1-NONSCATTER'].data
+	#mcrx = pyfits.open(fle)
+	#data = mcrx['CAMERA1-NONSCATTER'].data
 	print shape(data)
-	pyfits.writeto('/nobackupp2/rcsimons/sunrise_testing/jwst/'+fname_fits, data, clobber = True)
-
+	#pyfits.writeto('/nobackupp2/rcsimons/sunrise_testing/jwst/'+fname_fits, data, clobber = True)
+	os.system('cp '+fle+' /nobackupp2/rcsimons/sunrise_testing/jwst/'+fname+'_mcrx.fits')
 
 	print '/nobackupp2/rcsimons/sunrise_testing/jwst/'+fname_fits
 	'''
