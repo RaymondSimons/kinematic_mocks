ba5ab5e710e07e2324c99fa0f97471ef57374239
RaymondSimons <rsimons@pha.jhu.edu>
2016-08-25 12:25:29 -0400

    fix

--- save_cubes.py
+++ save_cubes.py
@@ -5,7 +5,7 @@ from numpy import *
 files = ['VELA28_a0.%3i_sunrise'%(num) for num in arange(300,380,10)]
 simdir = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA28'
 for fname in files:
-	print fname
+	print simdir+'/'+fname_fits+'/ifu/mcrx.fits'
 	fname_fits = fname+'_cam1.fits'
 	fle = glob.glob(simdir+'/'+fname_fits+'/ifu/mcrx.fits')
 	print fle
