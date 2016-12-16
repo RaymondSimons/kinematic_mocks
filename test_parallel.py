56beeffd3a4fa006f82730fe5f5c7778207de129
RaymondSimons <rsimons@pha.jhu.edu>
2016-12-16 16:23:01 -0500

    test parallel

--- test_parallel.py
+++ test_parallel.py
@@ -4,23 +4,23 @@ import astropy
 
 def test(x = 5):
     print x
-    for i in range(100000000): pass
+    for i in range(1000000000): pass
     f = open('./temp/'+str(x), 'w+')
     f.close()
 
 def test2(x = 10):
     print x
-    for i in range(100000000): pass
+    for i in range(1000000000): pass
     f = open('./temp/new_'+str(x), 'w+')
     f.close()
 
 a = time.time()
-Parallel(n_jobs = -1)(delayed(test)(i) for i in range(10))
+Parallel(n_jobs = -1)(delayed(test)(i) for i in range(8))
 b = time.time()
 print 'Finished parallel in:', b - a
 
 
 e = time.time()
-Parallel(n_jobs = 1)(delayed(test2)(i) for i in range(10))
+Parallel(n_jobs = 1)(delayed(test2)(i) for i in range(8))
 f = time.time()
 print 'Finished series in:', f - e
