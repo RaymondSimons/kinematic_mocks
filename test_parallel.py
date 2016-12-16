30e2e2ec14cb1926904a1d5d10972457456e2178
RaymondSimons <rsimons@pha.jhu.edu>
2016-12-16 16:32:26 -0500

    test parallel

--- test_parallel.py
+++ test_parallel.py
@@ -3,14 +3,14 @@ import time
 import astropy
 
 def test(x = 5):
-    print x
     for i in range(100000000): pass
+    print x
     f = open('./temp/'+str(x), 'w+')
     f.close()
 
 def test2(x = 10):
-    print x
     for i in range(100000000): pass
+    print x
     f = open('./temp/new_'+str(x), 'w+')
     f.close()
 
