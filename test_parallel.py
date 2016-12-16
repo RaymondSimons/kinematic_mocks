66d87105d792ba43132f897577419ebb43bccef1
RaymondSimons <rsimons@pha.jhu.edu>
2016-12-16 16:15:16 -0500

    test parallel

--- test_parallel.py
+++ test_parallel.py
@@ -1,11 +1,14 @@
 from joblib import Parallel, delayed
 import time
-
+import astropt
 
 def test(x = 5):
-    for i in range(80000000): pass
-    print x
+    f = open('./temp/'+str(x), 'w+')
+    f.close()
 
+def test2(x = 10):
+    f = open('./temp/new_'+str(x), 'w+')
+    f.close()
 
 a = time.time()
 Parallel(n_jobs = -1)(delayed(test)(i) for i in range(10))
@@ -14,6 +17,6 @@ print 'Finished in:', b - a
 
 
 a = time.time()
-Parallel(n_jobs = 1)(delayed(test)(i) for i in range(10))
+Parallel(n_jobs = 1)(delayed(test2)(i) for i in range(10))
 b = time.time()
 print 'Finished in:', b - a
