28455bbdc131cbaa2fcd7553eae5a4fd5c2383fb
RaymondSimons <rsimons@pha.jhu.edu>
2017-01-26 16:30:56 -0500

    update

--- write_qsub.py
+++ write_qsub.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
 
     f.write('#PBS -S /bin/bash\n')
     f.write('#PBS -l select=1:ncpus=20:model=has\n')
-    f.write('#PBS -l walltime=01:00:00\n')
+    f.write('#PBS -l walltime=02:00:00\n')
     f.write('#PBS -q normal\n')
     f.write('#PBS -N momentum_measurements\n')
     f.write('#PBS -M rsimons@jhu.edu\n')
