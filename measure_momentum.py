ef79b4872176db6ee7da2061cd9004e8a3bf780c
RaymondSimons <rsimons@pha.jhu.edu>
2017-01-31 14:10:47 -0500

    update

--- measure_momentum.py
+++ measure_momentum.py
@@ -163,7 +163,8 @@ class momentum_obj():
         self.gas_j_cen  = array([self.gas_jx_cen, self.gas_jy_cen, self.gas_jz_cen])
         self.gas_j_mag  = sqrt(self.gas_jx_cen**2. + self.gas_jy_cen**2. + self.gas_jz_cen**2.)
 
-    def measure_potential(self, r_min = 0.1,  r_step1 = 0.1, r_cen1 = 5, r_step2 = 0.3,  r_cen2 = 15, r_step3 = 0.5, r_max = 30.):
+#    def measure_potential(self, r_min = 0.1,  r_step1 = 0.1, r_cen1 = 5, r_step2 = 0.3,  r_cen2 = 15, r_step3 = 0.5, r_max = 30.):
+    def measure_potential(self, r_min = 0.1,  r_step1 = 2, r_cen1 = 5, r_step2 = 5,  r_cen2 = 15, r_step3 = 10, r_max = 30.):
 
         print 'Measuring the potential...'
         center = self.ds.arr([self.cen_x, self.cen_y, self.cen_z], 'kpc')
@@ -384,7 +385,7 @@ if __name__ == "__main__":
     new_snapfiles = np.asarray(new_snapfiles)
 
     #Make Parallel, send 3 at a time to the node (reduce memory overhead)
-    Parallel(n_jobs = 2, backend = 'threading')(delayed(measure_momentum)(new_snapfiles[i], out_sim_dir, nir_cat, nir_disc_cat) for i in arange(len(new_snapfiles)))
+    Parallel(n_jobs = 1, backend = 'threading')(delayed(measure_momentum)(new_snapfiles[i], out_sim_dir, nir_cat, nir_disc_cat) for i in arange(len(new_snapfiles)))
 
     #for i in arange(len(new_snapfiles)):
     #    measure_momentum(new_snapfiles[i], out_sim_dir, nir_cat, nir_disc_cat)
