42657f84217c037edc9cb069796ee1ff3c1b0bc6
RaymondSimons <rsimons@pha.jhu.edu>
2017-01-31 11:34:42 -0500

    update

--- write_qsub.py
+++ write_qsub.py
@@ -37,7 +37,7 @@ if __name__ == "__main__":
     f = open(fname, 'w+')
     f.write('#PBS -S /bin/bash\n')
     f.write('#PBS -l select=1:ncpus=20:model=has\n')
-    f.write('#PBS -l walltime=02:00:00\n')
+    f.write('#PBS -l walltime=03:00:00\n')
     f.write('#PBS -q normal\n')
     f.write('#PBS -N %s_all_momentum\n'%gal)
     f.write('#PBS -M rsimons@jhu.edu\n')
