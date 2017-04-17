abf273cbf6b5675968645cbb1483a35ceabc51c0
RaymondSimons <rsimons@pha.jhu.edu>
2017-04-17 12:56:25 -0400

    fix

--- make_kin_fits.py
+++ make_kin_fits.py
@@ -266,7 +266,7 @@ if __name__ == '__main__':
         sc_loc = abspaths[n].find('_a')
         scales.append(float(abspaths[n][sc_loc+2:sc_loc+7]))
 
-    print scales, abspath
+    print scales, abspaths
 
 
     #print path_to_mcrx, abspaths
