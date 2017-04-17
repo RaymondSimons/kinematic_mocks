bb40d30833f371590c44067549eda6dfaba5b46f
RaymondSimons <rsimons@pha.jhu.edu>
2017-04-17 12:48:32 -0400

    fix

--- make_kin_fits.py
+++ make_kin_fits.py
@@ -262,7 +262,7 @@ if __name__ == '__main__':
 
     for n, fl in enumerate(mcrx_files):
         print fl
-        #abspaths.append(os.path.abspath(fl))
+        abspaths.append(os.path.abspath(fl))
         scales.append(os.path.dirname(abspaths[n]).split('/')[1].split('_').strip('a'))
 
     print scales
