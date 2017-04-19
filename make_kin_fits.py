964258ca2aaccb8726143e437a05f32ebbdc6f63
RaymondSimons <rsimons@pha.jhu.edu>
2017-04-19 18:05:47 -0400

    testing

--- make_kin_fits.py
+++ make_kin_fits.py
@@ -302,8 +302,8 @@ def run_kin_fits(abspath, scale, kmap_name, gal, outdir, mcrx_data):
     #generate primary header for fits file
     prihdr = fits.Header()
     prihdr['COMMENT'] = "Storing the kinematic maps in this FITS file."
-    prihdr['VELA_name'] = gal
-    prihdr['kmap_name'] = kmap_name
+    prihdr['name'] = gal
+    prihdr['filename'] = kmap_name
     prihdr['ncams'] = str(ncams)
 
     prihdr['z'] = (str(1./scale - 1), 'redshift (w/ %s)'%cosmo.name)
