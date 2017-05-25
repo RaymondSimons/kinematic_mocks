6afabb9d77fcdffc3a595fd1acf21d5ca6244d85
RaymondSimons <rsimons@jhu.edu>
2017-05-25 15:52:01 -0400

    pulling image pa from fits files

--- write_image_pa.py
+++ write_image_pa.py
@@ -15,14 +15,13 @@ for i in arange(1,2):
             s_strt = fl.find('_sunrise.tar')-3
             s_stop = fl.find('_sunrise.tar')
             scale = fl[s_strt:s_stop]
-            f.write(scale + '\t')
             with tarfile.open(fl) as tf:
                 for SB in ['SB25', 'SB27', 'SB00']:
                     for inst in ['ACS-F606W','ACS-F775W', 'ACS-F850W', 'WFC3-F105W', 'WFC3-F125W', 'WFC3-F160W']:
                         for cam_n in arange(19):
                             fits_name = 'images_VELA%.2i_a0.%s_sunrise/VELA%.2i_a0.%s_sunrise_cam%.2i_%s_%s.fits'%(i, scale, i, scale, cam_n, inst, SB)
                             print fits_name
-                            f.write(SB+'\t'+inst+'\t'+'%.2i'%cam_n+'\t\n')
+                            f.write(scale + '\t'+ SB+'\t'+inst+'\t'+'%.2i'%cam_n+'\t\n')
                             #fits_file = tf.getmember(tf)    for entry in tf:
             f.write('\n')
 
