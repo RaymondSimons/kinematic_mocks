0dc83cec32d9c96eee417345afaaa4a7d80f1afd
RaymondSimons <rsimons@jhu.edu>
2017-05-25 15:31:50 -0400

    pulling image pa from fits files

--- write_image_pa.py
+++ write_image_pa.py
@@ -6,8 +6,9 @@ from glob import glob
 
 for i in arange(1,35):
     if i!=18:
-        f = open('/nobackupp2/rcsimons/catalogs/image_cats/VELA%.2i_image.cat'%(i))
+        f = open('/nobackupp2/rcsimons/catalogs/image_cats/VELA%.2i_image.cat'%(i),'w+')
         path = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA%.2i'%(i)
         fls = glob(path+'/*sunrise.tar')
         for fl in fls:
             print fl
+        f.close()
