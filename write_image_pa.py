ec3cfbaafcfc479f2b740278833efcb24c12f1ed
RaymondSimons <rsimons@jhu.edu>
2017-05-25 15:30:23 -0400

    pulling image pa from fits files

---
+++ write_image_pa.py
@@ -0,0 +1,13 @@
+import pyfits
+import tarfile
+import glob
+from glob import glob
+
+
+for i in arange(35):
+    if i!=18:
+        f = open('/nobackupp2/rcsimons/catalogs/image_cats/VELA%.2i_image.cat'%(i))
+        path = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA%.2i'%(i)
+        fls = glob(path+'/*sunrise.tar')
+        for fl in fls:
+            print fl
