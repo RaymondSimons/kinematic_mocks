1f285293c2f230de759bae2285449f5e2f74cf16
RaymondSimons <rsimons@jhu.edu>
2017-06-13 11:52:06 -0400

    change sensitivity

--- make_kin_fits.py
+++ make_kin_fits.py
@@ -389,7 +389,7 @@ def run_kin_fits(abspath, scale, kmap_name, gal, outdir, mcrx_data, arc_per_pixe
         print 'linear fov', kmap.cube_hdr['linear_fov']
         npix_new = ceil((kmap.cube_hdr['linear_fov']*cosmo.arcsec_per_kpc_proper(2).value)/arc_per_pixel/2.)*2.
         kmap.rebin_and_dim([npix_new, npix_new])
-        kmap.generate_blurred_map(kernel_size_arc = 0.8/2.35) #jwst
+        kmap.generate_blurred_map(kernel_size_arc = 0.85/2.35) #jwst
         #kmap.generate_blurred_map(kernel_size_arc = 0.05/2.35)
             
 
