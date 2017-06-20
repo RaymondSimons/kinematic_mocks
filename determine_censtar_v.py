5b91573be6e3e3e1a64c13f043fd1397750dc8e5
RaymondSimons <rsimons@jhu.edu>
2017-06-20 13:59:08 -0400

    add

--- determine_censtar_v.py
+++ determine_censtar_v.py
@@ -121,7 +121,7 @@ if __name__ == "__main__":
     for i in arange(len(new_snapfiles)):
         print new_snapfiles[i]
         if float(anames[i]) > 0.2:
-            in_nir = where(nir_cat[:,0] == anames[i])
+            in_nir = where(float(nir_cat[:,0].strip('a')) == anames[i])
             nir_cat_new = nir_cat[in_nir]
             cen_x, cen_y, cen_z, cen_vx, cen_vy, cen_vz, cen_star_offset_x, cen_star_offset_y, cen_star_offset_z, cen_star_voffset_x, cen_star_voffset_y, cen_star_voffset_z  = determine_center(new_snapfiles[i], nir_cat_new)
             out_cat.write('%5s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\t%10s\n'%(anames[i], cen_x, cen_y, cen_z, cen_vx, cen_vy, cen_vz, cen_star_offset_x, cen_star_offset_y, cen_star_offset_z, cen_star_voffset_x, cen_star_voffset_y, cen_star_voffset_z))
